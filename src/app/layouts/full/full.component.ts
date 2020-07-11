10746282cfe5568a8d445667e7c03c59a6136d4b
jortizm <dev_julian@outlook.es>
2020-07-10 21:20:42 -0500

    borrar publicación

--- src/app/layouts/full/full.component.ts
+++ src/app/layouts/full/full.component.ts
@@ -43,62 +43,14 @@ export class FullComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.options.sidebartype = 'mini-sidebar';
     if (this.router.url === '/') {
       this.router.navigate(['/dashboard/classic']);
     }
     this.defaultSidebar = this.options.sidebartype;
-    this.handleSidebar();
+    
   }
 
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    this.handleSidebar();
-  }
 
-  handleSidebar() {
-    this.innerWidth = window.innerWidth;
-    switch (this.defaultSidebar) {
-      case 'full':
-      case 'iconbar':
-        if (this.innerWidth < 1170) {
-          this.options.sidebartype = 'mini-sidebar';
-        } else {
-          this.options.sidebartype = this.defaultSidebar;
-        }
-        break;
-
-      case 'overlay':
-        if (this.innerWidth < 767) {
-          this.options.sidebartype = 'mini-sidebar';
-        } else {
-          this.options.sidebartype = this.defaultSidebar;
-        }
-        break;
-
-      default:
-    }
-  }
-
-  toggleSidebarType() {
-    switch (this.options.sidebartype) {
-      case 'full':
-      case 'iconbar':
-        this.options.sidebartype = 'mini-sidebar';
-        break;
-
-      case 'overlay':
-        this.showMobileMenu = !this.showMobileMenu;
-        break;
-
-      case 'mini-sidebar':
-        if (this.defaultSidebar === 'mini-sidebar') {
-          this.options.sidebartype = 'full';
-        } else {
-          this.options.sidebartype = this.defaultSidebar;
-        }
-        break;
-
-      default:
-    }
-  }
+ 
 }
